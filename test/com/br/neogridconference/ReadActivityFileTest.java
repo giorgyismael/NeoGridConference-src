86e53e46a5d39e1d11ef353897ed774ffee78478
giorgy <giorgyismael@gmail.com>
2018-11-14 09:39:17 -0200

    Classes de teste criadas!

---
+++ test/com/br/neogridconference/ReadActivityFileTest.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.neogridconference;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author giorgy
+ */
+public class ReadActivityFileTest {
+    
+    public ReadActivityFileTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        ReadActivityFile activityFileTest = new ReadActivityFile();
+        
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of startReadActivityFile method, of class ReadActivityFile.
+     */
+    @Test
+    public void testStartReadActivityFile() {
+
+    }
+
+    /**
+     * Test of dividerNameEndTime method, of class ReadActivityFile.
+     */
+    @Test
+    public void testDividerNameEndTime() {
+
+    }
+
+    /**
+     * Test of addActivity method, of class ReadActivityFile.
+     */
+    @Test
+    public void testAddActivity_String_int() {
+
+    }
+
+    /**
+     * Test of addActivity method, of class ReadActivityFile.
+     */
+    @Test
+    public void testAddActivity_Activity() {
+
+    }
+
+    /**
+     * Test of removeActivity method, of class ReadActivityFile.
+     */
+    @Test
+    public void testRemoveActivity() {
+
+    }
+
+    /**
+     * Test of getActivitiesList method, of class ReadActivityFile.
+     */
+    @Test
+    public void testGetActivitiesList() {
+
+    }
+
+    /**
+     * Test of getByIdInActivitiesList method, of class ReadActivityFile.
+     */
+    @Test
+    public void testGetByIdInActivitiesList() {
+
+    }
+
+    /**
+     * Test of getLongerTimeActivity method, of class ReadActivityFile.
+     */
+    @Test
+    public void testGetLongerTimeActivity() {
+
+    }
+
+    /**
+     * Test of getShorterTimeActivity method, of class ReadActivityFile.
+     */
+    @Test
+    public void testGetShorterTimeActivity() {
+
+    }
+
+    /**
+     * Test of getTimeActivityWith30Min method, of class ReadActivityFile.
+     */
+    @Test
+    public void testGetTimeActivityWith30Min() {
+
+    }
+
+    /**
+     * Test of printList method, of class ReadActivityFile.
+     */
+    @Test
+    public void testPrintList() {
+
+    }
+    
+}
---
+++ test/com/br/neogridconference/ScheduleOfActivitiesTest.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.neogridconference;
+
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author giorgy
+ */
+public class ScheduleOfActivitiesTest {
+    
+    public ScheduleOfActivitiesTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addActivityInSchedule method, of class ScheduleOfActivities.
+     */
+    @Test
+    public void testAddActivityInSchedule() {
+
+    }
+
+    /**
+     * Test of addTrackDay method, of class ScheduleOfActivities.
+     */
+    @Test
+    public void testAddTrackDay() {
+
+    }
+
+    /**
+     * Test of addActivity method, of class ScheduleOfActivities.
+     */
+    @Test
+    public void testAddActivity() {
+
+    }
+
+    /**
+     * Test of addLouchTimeActivity method, of class ScheduleOfActivities.
+     */
+    @Test
+    public void testAddLouchTimeActivity() {
+
+    }
+
+    /**
+     * Test of addNetworkingEventActivity method, of class ScheduleOfActivities.
+     */
+    @Test
+    public void testAddNetworkingEventActivity() {
+
+    }
+
+    /**
+     * Test of addIncrementInControlTime method, of class ScheduleOfActivities.
+     */
+    @Test
+    public void testAddIncrementInControlTime() {
+
+    }
+
+    /**
+     * Test of businessRulesTimeLouch method, of class ScheduleOfActivities.
+     */
+    @Test
+    public void testBusinessRulesTimeLouch() {
+
+    }
+
+    /**
+     * Test of businessRulesactivityNetworks method, of class ScheduleOfActivities.
+     */
+    @Test
+    public void testBusinessRulesactivityNetworks() {
+
+    }
+
+    /**
+     * Test of businessRulesConvertTime method, of class ScheduleOfActivities.
+     */
+    @Test
+    public void testBusinessRulesConvertTime() {
+
+    }
+
+    /**
+     * Test of bussinessRulesTrackDay method, of class ScheduleOfActivities.
+     */
+    @Test
+    public void testBussinessRulesTrackDay() {
+
+    }
+
+    /**
+     * Test of printSchedule method, of class ScheduleOfActivities.
+     */
+    @Test
+    public void testPrintSchedule() {
+
+    }
+    
+}
